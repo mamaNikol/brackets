39e828b313f4f37552e8f37c6399bf080008c721
mamaNikol <belief1987@ya.ru>
2020-03-03 11:56:21 +0900

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    var array = bracketsConfig.map(function(a) {
+        return (a = a[0] + a[1]);
+    });
+    var length = str.length / 2;
+    for (var i = 1; i <= length; i++) {
+        for (var j = 0; j <= array.length; j++) {
+            str = str.replace(array[j], "");
+        }
+    }
+    return str.length === 0 ? true : false;
+};
